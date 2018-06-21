30094e40cd5fa3c22007493fe5a7c9d2dff66d02
Ravishankar <ravishankar_kakarla@hotmail.com>
2018-06-20 22:37:45 -0400

    Add project files.

---
+++ AzureCoreIntro/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureCoreIntro.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

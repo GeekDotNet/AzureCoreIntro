44c23562356437440d650c15111365a391516269
Ravishankar <ravishankar_kakarla@hotmail.com>
2018-06-23 18:26:00 -0400

    Updating ConnectionString

--- AzureCoreIntro/Program.cs
+++ AzureCoreIntro/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using AzureCoreIntro.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace AzureCoreIntro
@@ -14,7 +17,9 @@ namespace AzureCoreIntro
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+            MigrateDatbase(host);
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
@@ -22,5 +27,15 @@ namespace AzureCoreIntro
                 .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .Build();
+
+        public static void MigrateDatbase(IWebHost webHost)
+        {
+            using (var scope = webHost.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                context.Database.Migrate();
+
+            }
+        }
     }
 }
